d93a5c0d2a609a30147e0b575c3b0b2d4bd08756
roman666123 <128031819+roman666123@users.noreply.github.com>
2023-10-23 21:27:35 +0800

    Add files via upload

---
+++ PiniTuGame/src/APP.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class APP {
+    public static void main(String[] args) {
+        new LogJFrame();
+        JFrame jf  = new JFrame();
+        jf.setSize(603,680);
+        jf.setTitle("test");
+        jf.setVisible(true);
+    }
+}
