c95ee398aeb201152dfc8f049111b0a88465579d
roman666123 <128031819+roman666123@users.noreply.github.com>
2023-10-23 21:24:33 +0800

    day_one

---
+++ src/RegisterJFrame.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class RegisterJFrame extends JFrame {
+    private String UserName;
+    private String UserPassword;
+    public RegisterJFrame(){
+        this.setSize(600,680);
+        this.setTitle("账号注册");
+        this.setAlwaysOnTop(true);
+    }
+}
