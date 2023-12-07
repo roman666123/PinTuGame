2e571db7dbac81b899362f5d023bcdf3ad5e650a
董哈哈 <3290577740@qq.com>
2023-12-07 20:07:49 +0800

    进行了步数的修改,一些bug的修复,完善了菜单中重新游戏和重新登录的功能

--- src/PinTuGame/BeginJFrame.java
+++ src/PinTuGame/BeginJFrame.java
@@ -2,11 +2,10 @@ package PinTuGame;
 
 import javax.swing.*;
 import javax.swing.border.BevelBorder;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.awt.event.*;
 import java.util.Random;
 
-public class BeginJFrame extends JFrame implements KeyListener {
+public class BeginJFrame extends JFrame implements KeyListener, ActionListener {
     private int x = 0, y = 0;
     private int[][] temp2 = new int[4][4];
     private String path = "image\\animal\\animal3\\";
@@ -68,30 +67,34 @@ public class BeginJFrame extends JFrame implements KeyListener {
         jb.setBounds(36, 6, 508, 560);
         this.getContentPane().add(jb);
         JLabel step = new JLabel("步数:" + stepcount);
-        step.setBounds(50, 30, 100, 20);
+        step.setBounds(500, 20, 100, 20);
         this.getContentPane().add(step);
     }
 
     int stepcount = 0;
+    JMenuItem ji1 = new JMenuItem("重新游戏");
+    JMenuItem ji2 = new JMenuItem("重新登陆");
+    JMenuItem ji3 = new JMenuItem("关闭游戏");
+    JMenuItem ji4 = new JMenuItem("公众号");
 
     private void initBar() {
         JMenuBar jb = new JMenuBar();
         JMenu jm = new JMenu("功能");
         JMenu jm1 = new JMenu("快捷键");
         JMenu jm2 = new JMenu("关于我们");
-        JMenuItem ji = new JMenuItem("更换图片");
-        JMenuItem ji1 = new JMenuItem("重新游戏");
-        JMenuItem ji2 = new JMenuItem("重新登陆");
-        JMenuItem ji3 = new JMenuItem("关闭游戏");
-        JMenuItem ji4 = new JMenuItem("公众号");
-
-        //向上添加条目
-        jm.add(ji);
+//        JMenuItem ji = new JMenuItem("更换图片");
+        //向上添加条目到相应的位置上面
+//        jm.add(ji);
         jm.add(ji1);
         jm.add(ji2);
         jm.add(ji3);
         jm2.add(ji4);
-
+        //给相关条目添加动作监听事件
+        ji1.addActionListener(this);
+        ji2.addActionListener(this);
+        ji3.addActionListener(this);
+        ji4.addActionListener(this);
+        //添加到jb上面
         jb.add(jm);
         jb.add(jm1);
         jb.add(jm2);
@@ -112,7 +115,6 @@ public class BeginJFrame extends JFrame implements KeyListener {
         //取消默认的居中放置,只有取消了才会按照xy轴的形式添加组件
         //隐藏的容器不需要创建对象,这是一个隐藏窗体
         this.setLayout(null);
-//        this.setBounds(null);
         this.addKeyListener(this);
     }
 
@@ -208,5 +210,26 @@ public class BeginJFrame extends JFrame implements KeyListener {
             }
         }
         return true;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object ob = e.getSource();
+        if (ob == ji1) {
+            //计步器清零
+            stepcount =0;
+            //胜利判断布尔变量进行重置
+            stark = false;
+            this.getContentPane().removeAll();
+            //重新加载图片
+            SetPicture();
+            this.getContentPane().repaint();
+        } else if (ob == ji2) {
+            System.out.println("2");
+        } else if (ob == ji3) {
+            System.out.println("3");
+        } else if (ob == ji4) {
+            System.out.println("4");
+        }
     }
 }
